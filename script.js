6e27dfb7423002b088122a18b11b1a7b0e01761c
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-08-23 22:29:25 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Example JavaScript functionality
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Page loaded and ready.');
-    // Add any additional JavaScript functionality here if needed
-});
